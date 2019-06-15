147cee6f537cac88eb0a69f5019e0b3b5299d729
Amritpal Singh <singhamritpal49@gmail.com>
2019-06-15 18:59:17 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,16 @@
+require 'pry'
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_hash = {}
+    data.each do |property, hash|
+      hash.each do |attribute, array|
+        array.each do |name|
+          if !new_hash.has_key?(name)
+
+            new_hash[name] = {:color => [], :gender => [], :lives => []}
+            binding.pry
+          end
+
+end
+end
+end
 end
